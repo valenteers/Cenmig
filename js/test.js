813c0562ab4420bd483b8471e3bbabfb26a5da0f
westwood13th <31763342+westwood13th@users.noreply.github.com>
2020-12-03 16:28:07 +0700

    Replace with file from 'that' usb

---
+++ js/test.js
@@ -0,0 +1,23 @@
+// let a = document.querySelectorAll('page');
+// for(let i in a) {
+//     console.log(i);
+// }
+// $(document).ready( function() {
+// var node = document.getElementById('box').childNodes
+
+// })
+// function clickme(el) {
+//     $('.page').hide();
+//     let page = document.getElementById(el);
+//     page.style.display = 'block';
+//     console.log(page.style.display);
+// }
+
+window.onsroll = function() {
+    stick();
+}
+
+$(document).ready(function(){
+    console.log($('.menu_panel').offset());
+}
+)
